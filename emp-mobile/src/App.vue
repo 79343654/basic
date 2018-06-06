bc808e47668b0e4aafd136377a6e7d6d3c161706
gemdale <303477418@qq.com>
2018-06-06 23:42:50 +0800

    2

---
+++ emp-mobile/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Index></Index>
+  </div>
+</template>
+
+<script>
+  import { Button } from 'mint-ui'
+  import Index from "./components/index"
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    Button,
+    Index
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
